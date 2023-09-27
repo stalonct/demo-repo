b12f561bf2e33de40b4cf277308d2fdbbb6b3d53
Stalon Thomas <stalon.thomas@innovaturelab.com>
2023-09-27 15:31:10 +0530

    Updated app.js from test1

--- demo-app/src/App.js
+++ demo-app/src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Haii, Helloo...</p>
+        <p>Haii from test branch</p>
       </header>
     </div>
   );
